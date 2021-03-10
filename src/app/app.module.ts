a4c34c7857801ed3b2fc88bc363d9bfd5134e7be
João Batista <jbspinola@stefanini.com>
2021-03-10 01:21:25 -0300

    Ajustes na integracao com keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,46 +12,47 @@ import {NgbAlertModule, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
 import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
-// function initializeKeycloak(keycloak: KeycloakService) {
-//   return () =>
-//       keycloak.init({
-//         config: {
-//           url: 'http://localhost:8080/auth',
-//           realm: 'your-realm',
-//           clientId: 'your-client-id',
-//         },
-//         initOptions: {
-//           onLoad: 'check-sso',
-//           silentCheckSsoRedirectUri:
-//               window.location.origin + '/assets/silent-check-sso.html',
-//         },
-//       });
-// }
+function initializeKeycloak(keycloak: KeycloakService) {
+    return () =>
+        keycloak.init({
+            config: {
+                url: 'http://localhost:8082/auth',
+                realm: 'sisvendas',
+                clientId: 'sisvendas-web',
+            },
+            initOptions: {
+                onLoad: 'login-required',
+                checkLoginIframe: true
+            },
+        });
+}
+
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    ToastrModule.forRoot(),
-    NgbPaginationModule,
-    NgbAlertModule,
-    AppRoutingModule,
-    KeycloakAngularModule,
-    FontAwesomeModule
-  ],
-  providers: [
-  //     {
-  //   provide: APP_INITIALIZER,
-  //   useFactory: initializeKeycloak,
-  //   multi: true,
-  //   deps: [KeycloakService],
-  // }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        ToastrModule.forRoot(),
+        NgbPaginationModule,
+        NgbAlertModule,
+        AppRoutingModule,
+        KeycloakAngularModule,
+        FontAwesomeModule
+    ],
+    providers: [
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeKeycloak,
+            multi: true,
+            deps: [KeycloakService],
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
